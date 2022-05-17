c450d222fc7a4c68c01ff4ed51f5614ee4f56c97
pikachu0310 <pikachu0310@DESKTOP-3PRPGRV.localdomain>
2022-05-17 21:37:35 +0900

    5000兆円欲しい！

--- works/counter.js
+++ works/counter.js
@@ -1,6 +1,19 @@
 let count = 0
+let mozi = ""
 const countUp = () => {
   count++
   const countElement = document.querySelector('#count')
-  countElement.innerText = '回数: ' + count
+  countElement.innerText = '回数: ' + count + mozi
+}
+const countReset = () => {
+    count = 0
+    mozi = ""
+    const countElement = document.querySelector('#count')
+    countElement.innerText = '回数: ' + count + mozi
+}
+const countSuper = () => {
+    count += 500000000000
+    mozi = "円"
+    const countElement = document.querySelector('#count')
+    countElement.innerText = '回数: ' + count + mozi
 }
